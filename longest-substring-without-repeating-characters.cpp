b9a54966e2ae1df75a39a9ce4a4098b02f22b22c
LiuYuan-SHU <2787141886@qq.com>
2022-03-07 23:42:27 +0800

    longest-substring-without-repeating-characters: create

---
+++ longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using namespace std; 
+
+int lengthOfLongestSubstring(string s)
+{
+	string* substring = nullptr;
+	string newString;
+}
+
+int main()
+{
+	string input;
+	cout << lengthOfLongestSubstring(input);
+	
+	return 0;
+}
