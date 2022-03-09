de04a4376d6a405c8e9cb246ad8aeb4bd07dc17a
LiuYuan-SHU <2787141886@qq.com>
2022-03-09 11:37:49 +0800

    longestPalindrome: finished note

--- README.md
+++ README.md
@@ -89,3 +89,33 @@ Then the loop begins:
 
     Update the last occurrence of the char
 
+### longest-palindromic-substring
+
+#### [Question](https://leetcode-cn.com/problems/longest-palindromic-substring/)
+
+#### [Solution](./longest-palindromic-substring.cpp)
+
+![image-20220309204148927](README.assets/image-20220309204148927.png)
+
+<center><b>Result</b></center>
+
+#### Idea
+
+take this string as example:
+
+![image-20220309205837603](README.assets/image-20220309205837603.png)
+
+1. In the first part of the loop, `left` will not move, while `right` move to the first char different from `s[left]`:
+
+    ![image-20220309210106110](README.assets/image-20220309210106110.png)
+
+2. Record `right` as `nextLeft`, which means the next position of left, for it is easy to prove that, ***neither character between left and right is the beginning of the next longer substring***. 
+
+    ![image-20220309210936064](README.assets/image-20220309210936064.png)
+
+3. Since the chars between `left` and `right` are the same, we can consider them as the center of the palindromic substring. We can move left and right at the same time, compare the chars they point
+
+    ![image-20220309211152374](README.assets/image-20220309211152374.png)
+
+
+
--- longest-palindromic-substring.cpp
+++ longest-palindromic-substring.cpp
@@ -5,26 +5,44 @@ using namespace std;
 
 string longestPalindrome(string s) 
 {
+	//`head` points the first char of the longest p-substring
+	//`tail` points the last char of the longest p-substring
+	//`max_length` records   length of thelogest p-substring
 	size_t head = 0,tail = 0,max_len = 0;
+	//`left` points the left side of current p-subsreing
+	//`right` points the right side of current p-substring
+	//`nextLeft` records the next value of `left`
 	int left = 0,right = 0,nextLeft = 0;
 	
 	while(true)
 	{
+		//after this loop, chars on sides of current p-substring are different from `s[left]` 
 		while(s[left] == s[right] && right < s.size()) { right++; }
-		nextLeft = right;
+		//the chars between `left` and `right` are the same
+		//so it's easy to prove that the first probable head of the new p-substring is `right`
+		nextLeft = right;			//Record the position where the next loop starts
+		//since `right` now points the first different char
+		//`right--` to make it points the last same char
 		right--;
+			
+		//Center extension algorithm
 		while(left >= 0 && right < s.size() && s[left] == s[right])
 		{
+			//if the length of current p-substring is longer
+			//record the new head, tail, max_len
 			if(max_len < right - left + 1)
 			{
 				max_len = right - left + 1;
 				head = left;
 				tail = right;
 			}
+			//extend window
 			left--;
 			right++;
 		}	
+		//set points to the position where the next loop starts
 		left = right = nextLeft;
+		//if left reachs the end of the string, traversal is finished
 		if(left == s.size()) { break; }	
 	}
 	
