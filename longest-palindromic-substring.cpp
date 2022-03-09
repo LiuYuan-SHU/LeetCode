25b7f3ac00f78fce0b19a33d1706b78e808062b2
LiuYuan-SHU <2787141886@qq.com>
2022-03-09 11:37:49 +0800

    longestPalindrome: commited but algorithm needs improvement
    core algorithm:
    
    		a b c b a
    
    when iterator points 'c', it compares the left char to the right char of
    it at the same time
    
    However forget to compare itself
    Improvement:
    before core algorithm, traverse the string and save the longest
    substring of the same char as `result`
    
    finished handling:
    	1. "aa"
    	2. "abb"
    	3. "aba"
    	4. "ababcba"
    failed to handle:
    	1. "aaaaaaaa"

---
+++ longest-palindromic-substring.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string longestPalindrome(string s) 
+{
+	if(s.size() == 0 || s.size() == 1)
+	{
+		return s;
+	}
+	else if(s.size() == 2)
+	{
+		return s[0] == s[1] ? s : string(1,s[0]);
+	}
+	
+	string* result = new string();
+	const char* str = s.c_str();
+	int offset = 0;
+	
+	for(size_t i = 1; i < s.size(); offset = 0, i++)
+	{
+		if(str[i - 1] == str[i] && result->size() < 2)
+		{
+			result = new string(2, str[i - 1]);
+		}
+		do 
+		{
+			offset++;
+		}while(offset <= i && offset < (s.size() - i) && str[i - offset] == str[i + offset]);
+		offset--;
+		result = result->size() >= (1 + 2 * offset) ? result : (new string(&str[i - offset],2 * offset + 1));
+	}
+	
+	return *result;
+}
+
+void test(string input)
+{
+	cout << longestPalindrome(input) << endl;
+}
+
+int main()
+{
+	test("a");
+	test("abb");
+	test("aba");
+	test("abaabcdcba");
+	test("bbac");
+}
